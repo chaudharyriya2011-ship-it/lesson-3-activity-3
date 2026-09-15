97f928f5d31a81db37580b60796d6f3432b9a799
chaudharyriya2011-ship-it <chaudharyriya2011@gmail.com>
2026-09-15 21:37:59 +0530

    lesson 3

---
+++ main2.py
@@ -0,0 +1,11 @@
+name = "hello"
+age = 12
+is_student = True
+weight = 21.2
+print(name , age , is_student , weight )
+print(type(name) , type(age) , type(is_student) , type(weight))
+print("type casting starting")
+age = str(age)
+print(type(age))
+weight = int(weight) 
+print(type(weight))
